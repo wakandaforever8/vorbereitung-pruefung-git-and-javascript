1d7e579111523af325da4eee26ada1abcd5d8174
wakandaforever8 <abinayasureshkumar@yahoo.com>
2022-12-04 19:11:33 +0100

    Added gravity

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const circle = document.querySelector("#circ")
+
+
 function start() {
     window.requestAnimationFrame(update)
 }
 
 function update() {
-    console.log("Hello World")
-
+    let cy = parseFloat(circle.getAttribute("cy"))
+    cy = cy +1
+    circle.setAttribute("cy" , cy)
     window.requestAnimationFrame(update)
 }
