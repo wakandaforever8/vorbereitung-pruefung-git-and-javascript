736404d5cb3df6f698f720874c02eab78ebf1f8a
wakandaforever8 <abinayasureshkumar@yahoo.com>
2022-12-04 19:14:37 +0100

    Beschleunigung

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const circle = document.querySelector("#circ")
-
+let dy = 0
 
 function start() {
     window.requestAnimationFrame(update)
 }
 
 function update() {
+    dy = dy + 0.05
+
+
     let cy = parseFloat(circle.getAttribute("cy"))
-    cy = cy +1
+    cy = cy + dy
     circle.setAttribute("cy" , cy)
+
     window.requestAnimationFrame(update)
 }
