8f89a781aa27f8183ee0aecd60ea5910fd5d0c41
wakandaforever8 <abinayasureshkumar@yahoo.com>
2022-12-07 16:31:58 +0100

    Bounce hinzugefügt

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function start() {
 }
 
 function update() {
+    
     dy = dy + 0.05
 
-
     let cy = parseFloat(circle.getAttribute("cy"))
+
+    if (cy > 100) {
+        dy = dy * (-1)
+    } 
+
+
     cy = cy + dy
     circle.setAttribute("cy" , cy)
 
